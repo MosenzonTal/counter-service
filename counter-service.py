b6b6b3e5fb60207308f9d6661776884cc14ab325
talmosenzon <talmosenzon@gmail.com>
2022-06-29 12:00:30 +0300

    added GET counter feature

--- counter-service.py
+++ counter-service.py
@@ -1,14 +1,21 @@
 #!flask/bin/python
 from flask import Flask, request, request_started
+import os
+
 app = Flask(__name__)
-counter = 0
+post_counter = 0
+get_counter = 0
+
+
 @app.route('/', methods=["POST", "GET"])
 def index():
-    global counter
+    global post_counter, get_counter
     if request.method == "POST":
-        counter+=1
-        return "Hmm, Plus 1 please "
+        post_counter += 1
+        return "Hmm, Plus 1 POST request "
     else:
-        return str(f"The counter is: {counter} ")
+        get_counter += 1
+        return str(f"The POST counter is: {post_counter} <br> the GET counter is: {get_counter}")
+
 if __name__ == '__main__':
-    app.run(debug=True,port=80,host='0.0.0.0')
+    app.run(debug=True, port=80, host='0.0.0.0')
